94123a0485bd2f8a9e0b0c70d8f52264036ca941
Eduardo cerato <eduardo.cerato@geradordedevs.com.br>
2023-01-26 20:50:54 -0300

    Feat: alteração no banner, logo e título da landing page

--- app/index.php
+++ app/index.php
@@ -50,26 +50,17 @@ foreach ( $raw_categories as $category ) {
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-	<script src="assets/js/main.js"></script>
-</head>
-<body>
-
-</body>
-</html>
-
 <!doctype html>
 <html lang="en">
 	<head>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+		<script src="assets/js/main.js"></script>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name="description" content="">
@@ -90,15 +81,15 @@ foreach ( $raw_categories as $category ) {
 
 			<section class="text-center pb-3 shadow-bottom position-relative">
 				<div class="container">
-					<img src="assets/images/logo-jbs.svg">
-					<p class="h5 mb-2">Welcome to JBS Brand Catalogues</p>
+					<img src="assets/images/catalogo-friboi-logo.svg">
+					<p class="h5 mb-2">Bem vindo ao catálogo de marcas Friboi</p>
 					<a href="#" onClick="scrollToContent()" class="red mb-0">scroll to view <img src="assets/images/duo-down.svg"></a>
 				</div>
 			</section>
 
 			<section class="text-center pt-4 pb-2 position-relative">
 				<div class="container">
-					<img src="http://3.91.170.62/wp-content/uploads/2022/10/sial-22-jbs.jpg" class="w-100 max-height-300" id="banner-sial">
+					<img src="assets/images/convencao-friboi-banner.jpg" class="w-100 max-height-300" id="banner-sial">
 				</div>
 			</section>
 
